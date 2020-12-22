2e89a6f1acda9f5676969f4ce7c34ac5a3e183d6
bbuturak <berkbuturak@gmail.com>
2020-12-22 19:57:47 +0100

    Minor adjustments

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'webpage')))
-
 const PORT = 80 || process.env.PORT;
+
+app.get('/trainer', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'webpage/index.html'));
+
+});
 
 app.get('/', (req, res) => {
 
-    res.sendFile('index.html');
-
-});
+    res.send('beafowl.de');
+    res.end();
+})
 
 app.listen(PORT, () => { console.log(`Started server on port ${PORT}`)});
